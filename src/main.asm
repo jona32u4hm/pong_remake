873a62cc431499a0cfea963f36d7069390b21ffc
jona <78815552+jona32u4hm@users.noreply.github.com>
2025-12-18 21:01:27 -0600

    Revert "Revert "Feature/ball""

--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,18 @@
 INCLUDE "include/hardware.inc"
 
+
+def OBJ_Y_OFFSET EQU 2
+def TILE_DIMENTION EQU 8
+export OBJ_Y_OFFSET
+export TILE_DIMENTION
+
 SECTION "Main", ROM0
 
+
 Setup::
+
 	di	; disable interrupts
-	ld	SP, $FFFF
-	
+	ld	SP, $FFFF  
 	;-------- DMA Setup --------
 	ld	de, VBlankISR
 	ld	hl, DMACode
@@ -40,18 +47,51 @@ Setup::
 	cp %01
     jr nz, .waitVBlank 
 	ld	a, [rLCDC]
-	and LCDC_OFF	; turn off lcd by toggling 
+	and ~LCDC_ON 
 	ld	[rLCDC], a	
 	;load tiles
 	ld	hl, Tiles	
 	ld	de, $8000 ;vram tile mem
 	ld	bc, TilesEnd - Tiles
 	call	memLoad
+	;load vram map for tenniscourt
+	ld	hl, Tennis
+	ld	de, $9800
+	ld	bc, $1412 ;20 x 18 ($15 -1 x $13 -1)
+	call mapLoad
+
 	;turn LCD ON again
 	ld	a, [rLCDC]
 	or	LCDC_ON
 	ld	[rLCDC], a	
 
-
+	;set OAM shadow RAM to 0 (hides all objects)
+	ld a, 0 
+	ld de, OAM_Other
+	ld bc, setUp2Here - OAM_Other
+	call memSet
+	;intialize paddle and ball objects
+	ld	hl, PaddleObj	
+	ld	de, OAM_Source ;vram tile mem
+	ld	bc, PaddleObjEND - PaddleObj
+	call	memLoad
+	;setup paddle velocity to offsetted zero used for negative numbers
+	ld a, VELOCITY_ZERO_OFFSET
+	ld [velocityP1], a
+	ld [velocityP2], a
+	;setup ball variables:
+	ld a, low(launchingSetup)
+    ld [ballState], a
+    ld a, high(launchingSetup)
+    ld [ballState + 1], a
 	
-	jr @
+
+Main::
+	halt 
+	nop 
+
+	call readJoyPad
+	call updatePaddles
+	call updateBall
+
+	jr Main
